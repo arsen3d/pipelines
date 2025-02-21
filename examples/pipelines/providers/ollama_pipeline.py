d4209fd92d7be5dca965404bd20d6b404e0a0cf5
Arsen Yeremin <arsen3d@gmail.com>
2025-02-20 16:20:53 -0800

    Remove context from payload inputs in ollama_pipeline.py

--- examples/pipelines/providers/ollama_pipeline.py
+++ examples/pipelines/providers/ollama_pipeline.py
@@ -73,15 +73,14 @@ class Pipeline:
                 raise ValueError("Environment variable 'SK' is not set")
 
             # context = [{"role": msg["role"], "content": msg["content"]} for msg in messages]
-            context = [{"role": msg["role"], "content": msg["content"]} for msg in messages]
+            # context = [{"role": msg["role"], "content": msg["content"]} for msg in messages]
             payload = {
                 "pk": sk,
                 "module": "github.com/rhochmayr/ollama-deepseek-r1-7b:1.0.0",
-                "inputs": f'-i "Prompt={user_message} Context:{context}"',
+                "inputs": f'-i "Prompt={user_message}"',
                 "format": "json",
                 "stream": "true"
             }
-            print("payload", payload)
             response = requests.post(
                 "https://js-cli.arsenum.com",
                 headers={"Content-Type": "application/json"},
